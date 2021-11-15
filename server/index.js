df126ed6fd23888db148ee9f3aec1c9accfe3a99
clewarne15@gmail.com <clewarne15@gmail.com>
2021-11-14 16:49:31 -0800

    Created REST API and basic post request.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/code", (req,res) => {
+  res.json()
+});
+
+app.post("/sendCode/",(req,res) =>{
+  console.log("Hello World");
+  const newCode = res;
+
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
