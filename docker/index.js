1d39bfcdf12cd090dddc235ac791b915c6afcdfd
clewarne15@gmail.com <clewarne15@gmail.com>
2021-12-02 22:13:25 -0800

    Added a server in the docker container with APIs that allow for a get request to be made by the docker container to get code.

--- docker/index.js
+++ docker/index.js
@@ -1,2 +1,21 @@
-console.log("Hello World!");
-var input = [];
+const { appendFile } = require("fs");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// console.log("Hello World!");
+// var input = [];
+
+app.get("/codeToDockerTemp", (req, res) => {
+  res.json();
+  console.log(req.body.input);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
